739774d2d6ef69d6a2ca366f70e03a2718b02ad5
John Pirie <john@thepiries.net>
2022-07-17 22:14:28 -0400

    Techniques to find exact location data for upcoming intersection; flesh out in directions.py

--- directions.py
+++ directions.py
@@ -1,18 +1,38 @@
+"""
+Current code below is but a tinker toy.
+
+Process:
+Read in transformed.txt
+For each mapping set:
+    For each origin/waypoint/destination pair:
+        Refer to nearby.py for grabbing geocode info and particularly place_id, the unique identifier we want.
+        Call geocode on origin for origin geocode data (lat/lng right now)
+        Call nearby with origin & next waypt/destination to get known location result
+        Call geocode on destination for destination geocode data
+        Print out each value as discovered so we can see if/when directions go bad and have to be edited
+    Construct waypoints based on geocode place_id values
+    Use test.html as a template for a renderable browser page.
+    Insert into test.html the route, using place_id values to ensure all valid, resolved map points
+"""
+
 import googlemaps
 import json
 
 from config import working_dir, maps_key_fpath
 
 
+origin = 'Residence Inn by Marriott, 40 Lake Ave., Manchester, NH 03101'
+destination = 'Main St'
+
+
 with open(maps_key_fpath, 'r') as fp:
     api_key = fp.read()
 gmaps = googlemaps.Client(key=api_key)
 directions_result = gmaps.directions(
-    origin='3167 The Oaks Road, Ellicott City, MD',
-    destination='950 Cannon Circle, Webster, NY',
+    origin=origin,
+    destination=destination,
     mode='driving',
     waypoints=[
-        'Newark International Airport',
     ],
 )
 
--- nearby.py
+++ nearby.py
@@ -1,2 +1,37 @@
 # Test getting next waypoint with Maps API nearby location search
+import googlemaps
+import json
 
+
+from config import working_dir, maps_key_fpath
+
+
+with open(maps_key_fpath, 'r') as fp:
+    api_key = fp.read()
+gmaps = googlemaps.Client(key=api_key)
+
+origin_address = 'Residence Inn by Marriott, 40 Lake Ave., Manchester, NH 03101'
+destination_by_name = 'Lake Ave & Granite St'
+
+origin_geocode = gmaps.geocode(address=origin_address)
+origin_geo_json = json.dumps(origin_geocode, indent=4)
+print(f'Origin geocode:\n{origin_geo_json}')
+
+(origin_lat, origin_lng) = map(origin_geocode[0]['geometry']['location'].get, ('lat', 'lng'))
+
+response = gmaps.places_nearby(
+    location=(origin_lat, origin_lng),
+    keyword=destination_by_name,
+    rank_by='distance',
+)
+nearby_result = response.get('results')
+print(f'Nearby result:\n{nearby_result}')
+
+nearby_fpath = f'{working_dir}/nearby.json'
+with open(nearby_fpath, 'w') as fp:
+    json.dump(nearby_result, fp, indent=4)
+print(f'Wrote {nearby_fpath}.')
+
+destination_geocode = gmaps.geocode(place_id=nearby_result[0]['place_id'])
+destination_geo_json = json.dumps(destination_geocode, indent=4)
+print(f'Destination geocode:\n{destination_geo_json}')
