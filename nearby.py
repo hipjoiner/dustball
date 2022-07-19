2daa3047ab383e6c31d71fab7b5769632e70c876
John Pirie <hipjoiner@gmail.com>
2022-07-19 16:28:18 -0400

    Checkpoint; work on doing all legs, skipping forward over legs already done. Unresolvable nearbys a problem.

--- nearby.py
+++ nearby.py
@@ -1,7 +1,24 @@
 """Get next waypoint with Maps API nearby location search"""
 import json
+import os
+from time import sleep
 
 from config import working_dir, gmaps
+
+
+def get_distance(origin_geodata, destination_geodata):
+    orig = (origin_geodata['lat'], origin_geodata['lng'])
+    dest = (destination_geodata['lat'], destination_geodata['lng'])
+    data = gmaps.distance_matrix(
+        origins=orig,
+        destinations=dest,
+        mode='driving',
+        units='imperial',
+    )
+    # print(json.dumps(data, indent=4))
+    elem = data["rows"][0]["elements"][0]
+    result = f'{elem["distance"]["text"]} ({elem["duration"]["text"]})'
+    return result
 
 
 def get_geocode_vitals(address=None, place_id=None, name=''):
@@ -22,47 +39,77 @@ def get_geocode_vitals(address=None, place_id=None, name=''):
     }
 
 
-def get_nearby_id(origin, target):
-    response = gmaps.places_nearby(
-        location=(origin['lat'], origin['lng']),
-        keyword=target,
-        rank_by='distance',
-    )
-    result = response.get('results')
-    if not result:
-        raise ValueError(f'ERROR getting nearby for {origin} to {target}')
-    return result[0]['place_id']
-
-
-def get_point_names():
+def get_legs():
     trans_fpath = f'{working_dir}/transformed.txt'
     with open(trans_fpath, 'r') as fp:
         legs = json.load(fp)
-    # TODO: Just first leg for now; when we do all legs, save destination info from one leg as origin for next
-    data = legs[0]
-    result = [data['origin']]
-    for wayp in data['waypoints']:
-        result.append(wayp)
-    result.append(data['destination'])
-    return result
+    results = []
+    for leg in legs:
+        result = [leg['origin']]
+        for wayp in leg['waypoints']:
+            result.append(wayp)
+        result.append(leg['destination'])
+        results.append(result)
+    return results
+
+
+def get_nearby_id(origin, target):
+    """Call API to find nearby location we want, then extract place_id Maps's unique place identifier."""
+    result = None
+    count = 0
+    while not result and count < 5:
+        if count:
+            print('Get nearby failed; sleep...')
+            sleep(5)
+        count += 1
+        response = gmaps.places_nearby(location=(origin['lat'], origin['lng']), keyword=target, rank_by='distance')
+        result = response.get('results')
+    if not result:
+        raise ValueError(f'Failed to get nearby for {origin["address"]} to {target}')
+    return result[0]['place_id']
 
 
 if __name__ == '__main__':
-    points = get_point_names()
-    geodata = [{} for i in range(len(points))]
-    for i, destination_name in enumerate(points):
-        if i == 0:      # 1st pass, just get geocode of origin
-            geodata[i] = get_geocode_vitals(address=points[i], name=destination_name)
+    legs = get_legs()
+    alldata = []
+    last_dest_geo = None
+    for leg_i, points in enumerate(legs):
+        leg_no = leg_i + 1
+        leg_nearby_fpath = f'{working_dir}/nearby-{leg_no}.json'
+        if os.path.exists(leg_nearby_fpath):
+            with open(leg_nearby_fpath, 'r') as fp:
+                leg_data = json.load(fp)
+            last_dest_geo = leg_data[-1]
+            print(f'Leg {leg_no} already written ({leg_nearby_fpath}); skipping...')
             continue
-        print(f"{i}. From {geodata[i - 1]['address']} to {destination_name}...")
-        print(f'Origin geodata:\n{json.dumps(geodata[i - 1], indent=4)}')
-        nearby_place_id = get_nearby_id(geodata[i - 1], destination_name)
-        geodata[i] = get_geocode_vitals(place_id=nearby_place_id, name=destination_name)
-        print(f'Destination geodata:\n{json.dumps(geodata[i], indent=4)}')
-        print('')
-    print(f'Final results:\n{json.dumps(geodata, indent=4)}')
+        geodata = [{} for x in range(len(points))]
+        for dest_no, destination_name in enumerate(points):
+            if dest_no == 0:      # Origin; just get geocode and store
+                if last_dest_geo is None:
+                    geodata[dest_no] = get_geocode_vitals(address=points[dest_no], name=destination_name)
+                else:
+                    # For origin for this leg, use final destination from last leg
+                    geodata[dest_no] = last_dest_geo
+                continue
+            print(f"Leg {leg_no}, segment {dest_no}. From {geodata[dest_no - 1]['address']} to {destination_name}...")
+            # print(f'Origin geodata:\n{json.dumps(geodata[dest_no - 1], indent=4)}')
+            # print(f'Origin: {geodata[dest_no - 1]["address"]}')
+            nearby_place_id = get_nearby_id(geodata[dest_no - 1], destination_name)
+            # Is Google throttling when we make lots of calls quickly? Not sure.
+            geodata[dest_no] = get_geocode_vitals(place_id=nearby_place_id, name=destination_name)
+            print(f'Destination: {geodata[dest_no]["address"]}')
+            distance = get_distance(geodata[dest_no], geodata[dest_no - 1])
+            print(f'Distance: {distance}')
+            print('')
+            sleep(2)
+        with open(leg_nearby_fpath, 'w') as fp:
+            json.dump(geodata, fp, indent=4)
+        print(f'Wrote {leg_nearby_fpath}.')
+        last_dest_geo = geodata[-1].copy()
+        alldata.append(geodata)
+    print(f'Final results:\n{json.dumps(alldata, indent=4)}')
     nearby_fpath = f'{working_dir}/nearby.json'
     with open(nearby_fpath, 'w') as fp:
-        json.dump(geodata, fp, indent=4)
+        json.dump(alldata, fp, indent=4)
     print(f'Wrote {nearby_fpath}.')
 
--- transform.py
+++ transform.py
@@ -9,10 +9,12 @@ from config import working_dir
 
 
 do_not_divert = re.compile(re.escape('do not divert'), re.IGNORECASE)
+in_parens = re.compile('\(.+\)')
 
 
-def new_leg():
+def new_leg(leg_no):
     return {
+        'leg':         str(leg_no),
         'origin':      None,
         'waypoints':   [],
         'destination': None,
@@ -30,6 +32,16 @@ def parse_road(line):
         if loc != -1:
             tail = tail[loc + 3:]
     tail = do_not_divert.sub('', tail)
+    tail = in_parens.sub('', tail)
+    # Often, both street and route no. are listed, separated by "/". Isolate and choose route portion.
+    if '/' in tail:
+        toks = tail.split('/')
+        if any(char.isdigit() for char in toks[0]):
+            tail = toks[0]
+        elif any(char.isdigit() for char in toks[1]):
+            tail = toks[1]
+        else:
+            tail = toks[0]
     road = tail.strip(' ')
     return road
 
@@ -39,7 +51,7 @@ def parse_legs():
     with open(interpreted_fpath, 'r') as fp:
         lines = fp.read().split('\n')
     legs = []
-    leg = new_leg()
+    leg = new_leg(len(legs) + 1)
     prev_road = None
     for line in lines:
         if line.startswith('S'):
@@ -47,7 +59,7 @@ def parse_legs():
             if leg['origin']:
                 leg['destination'] = place
                 legs.append(leg)
-                leg = new_leg()
+                leg = new_leg(len(legs) + 1)
             if line != lines[-1]:
                 leg['origin'] = place
             else:
